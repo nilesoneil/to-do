165bbfa94cd12aad45db7619f9b5fcaee6ad6944
Niles O'Neil <nilesoneil@gmail.com>
2022-09-11 14:55:47 -0700

    constructor and prototype.addTask added
    Co-authored-by: Hans Ellis <bullhorn@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,72 @@ function List() {
   this.tasks = {};
 }
 
+//function Task(taskName) {
+  //this.taskName = taskName;
+//}
+
+List.prototype.addTask = function(taskName) {
+  this.task[taskName.theTask] = taskName
+}
+
+//List.prototype.fullList = 
+function List() {
+  this.taskList = {};
+  this.currentId = 0;
+}
+
+List.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+List.prototype.addTask = function(listItem) {
+  listItem.id = this.assignId();
+  this.taskList[listItem.id] = listItem
+}
+
+function Task(taskName) {
+  this.taskName = taskName;
+  
+}
+let ourList = new List();
+let task1 = new Task("hans");
+ourList.addTask(tasks);
+
+
+//////////////Niles' attempt
+function List() {
+  this.tasks = {};
+}
+
+List.prototype.addTask = function(errand) {
+  this.tasks[errand.taskName] = errand;
+};
+
 function Task(taskName) {
   this.taskName = taskName;
 }
 
-List.prototype.fullList = 
+
+
+let list = new List();
+let errand = new Task(["laundry", "washCar", "vacuum", "oilChange"]);
+list.addTask(errand);
+
+////////////bossman's////
+function TaskList() {
+  this.tasks = {};
+}
+
+TaskList.prototype.addTask = function(task) {
+  this.tasks[task.toString()] = task;
+}
+
+function Task(taskName) {
+  this.taskName = taskName;
+}
+
+
+let list = new TaskList();
+let tasks = new Task(["laundry", "washCar", "vacuum", "oilChange"]);
+list.addTask(tasks);
