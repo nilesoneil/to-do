9289fc84230045316bd91568840e49fbf161826e
Niles O'Neil <nilesoneil@gmail.com>
2022-09-11 15:57:57 -0700

    cleaned up scripts.js file
    Co-authored-by: Hans Ellis <bullhorn@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,4 @@
-function List() {
-  this.tasks = {};
-}
 
-//function Task(taskName) {
-  //this.taskName = taskName;
-//}
-
-List.prototype.addTask = function(taskName) {
-  this.task[taskName.theTask] = taskName
-}
-
-//business logic for List 
-function List() {
-  this.taskList = {};
-  this.currentId = 0;
-}
-
-List.prototype.addTask = function(listItem) {
-  this.taskList[listItem.id] = listItem;
-}
-
-List.prototype.assignId = function() {
-  this.currentId =+ 1;
-  return this.currentId;
-}
-
-//business logic for tasks
-function Task(taskName) {
-  this.taskName = taskName;
-}
-
-let newList = new List();
-let task1 = new Task("hans");
-let task2 = new Task("hans2");
-newList.addTask(task1);
-
-//////////////Niles' attempt
 function List() {
   this.tasks = {};
   this.currentId = 0;
@@ -51,51 +14,15 @@ List.prototype.assignId = function() {
   return this.currentId;
 };
 
-
 function Task(taskName) {
   this.taskName = taskName;
 }
 
 
-
+//test code
 let list = new List();
-let errand = new Task(["laundry", "washCar", "vacuum", "oilChange"]);
-list.addTask(errand);
-
-////////////bossman's////
-function TaskList() {
-  this.tasks = {};
-}
-
-TaskList.prototype.addTask = function(task) {
-  this.tasks[task.toString()] = task;
-}
-
-function Task(taskName) {
-  this.taskName = taskName;
-}
-
-
-let list = new TaskList();
-let tasks = new Task(["laundry", "washCar", "vacuum", "oilChange"]);
-list.addTask(tasks);
-
-////template before we added id's (safe code, return if needed)
-
-function List() {
-  this.tasks = {};
-}
-
-List.prototype.addTask = function(errand) {
-  this.tasks[errand.taskName] = errand;
-};
-
-function Task(taskName) {
-  this.taskName = taskName;
-}
-
-
-
-let list = new List();
-let errand = new Task(["laundry", "washCar", "vacuum", "oilChange"]);
+let errand = new Task("laundry");
+let errand2 = new Task("washCar");
+let errand3 = new Task("vacuum");
+let errand4 = new Task("oilChange");
 list.addTask(errand);
