33039318a62705ead3fb09b8be782e4637fb36a1
Niles O'Neil <nilesoneil@gmail.com>
2022-09-11 15:54:58 -0700

    Ids are added
    Co-authored-by: Hans Ellis <bullhorn@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -10,39 +10,47 @@ List.prototype.addTask = function(taskName) {
   this.task[taskName.theTask] = taskName
 }
 
-//List.prototype.fullList = 
+//business logic for List 
 function List() {
   this.taskList = {};
   this.currentId = 0;
 }
 
+List.prototype.addTask = function(listItem) {
+  this.taskList[listItem.id] = listItem;
+}
+
 List.prototype.assignId = function() {
-  this.currentId += 1;
+  this.currentId =+ 1;
   return this.currentId;
 }
 
-List.prototype.addTask = function(listItem) {
-  listItem.id = this.assignId();
-  this.taskList[listItem.id] = listItem
-}
-
+//business logic for tasks
 function Task(taskName) {
   this.taskName = taskName;
-  
 }
-let ourList = new List();
-let task1 = new Task("hans");
-ourList.addTask(tasks);
 
+let newList = new List();
+let task1 = new Task("hans");
+let task2 = new Task("hans2");
+newList.addTask(task1);
 
 //////////////Niles' attempt
 function List() {
   this.tasks = {};
+  this.currentId = 0;
 }
 
 List.prototype.addTask = function(errand) {
-  this.tasks[errand.taskName] = errand;
+  errand.id = this.assignId();
+  this.tasks[errand.id] = errand;
 };
+
+List.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+
 
 function Task(taskName) {
   this.taskName = taskName;
@@ -71,3 +79,23 @@ function Task(taskName) {
 let list = new TaskList();
 let tasks = new Task(["laundry", "washCar", "vacuum", "oilChange"]);
 list.addTask(tasks);
+
+////template before we added id's (safe code, return if needed)
+
+function List() {
+  this.tasks = {};
+}
+
+List.prototype.addTask = function(errand) {
+  this.tasks[errand.taskName] = errand;
+};
+
+function Task(taskName) {
+  this.taskName = taskName;
+}
+
+
+
+let list = new List();
+let errand = new Task(["laundry", "washCar", "vacuum", "oilChange"]);
+list.addTask(errand);
